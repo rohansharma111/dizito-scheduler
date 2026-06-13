2ac1bdc71bca9ed48bcb8d0d2c6e6ebff889f022
rohansharma111 <rohansharmadrs@gmail.com>
2026-06-14 00:33:57 +0530

    MULTI PAGE ACCOUNT CONNECTION log

--- app/api/meta/login/route.ts
+++ app/api/meta/login/route.ts
@@ -1,29 +1,23 @@
 export async function GET() {
+  const clientId = process.env.META_APP_ID;
 
-  const clientId =
-    process.env.META_APP_ID;
+  const redirectUri = process.env.META_REDIRECT_URI;
 
-  const redirectUri =
-    process.env.META_REDIRECT_URI;
-
-  const scope =
-    [
-      "pages_show_list",
-      "pages_manage_posts",
-      "pages_read_engagement",
-      "instagram_basic",
-      "instagram_content_publish",
-      "business_management"
-    ].join(",");
+  const scope = [
+    "pages_show_list",
+    "pages_manage_posts",
+    "pages_read_engagement",
+    "instagram_basic",
+    "instagram_content_publish",
+    "business_management",
+  ].join(",");
 
   const url =
     `https://www.facebook.com/v19.0/dialog/oauth` +
     `?client_id=${clientId}` +
-    `&redirect_uri=${encodeURIComponent(
-      redirectUri!
-    )}` +
+    `&redirect_uri=${encodeURIComponent(redirectUri!)}` +
     `&scope=${scope}`;
-
+  console.log("REDIRECT URI:", redirectUri);
+  console.log("OAUTH URL:", url);
   return Response.redirect(url);
-
 }
