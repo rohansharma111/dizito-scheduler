382d7dbe816968c8366dda4d0f26408318333d1c
rohansharma111 <rohansharmadrs@gmail.com>
2026-07-01 21:17:07 +0530

    Footer

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -24,29 +24,31 @@ export default function AppShell({
   const closeSidebar = () => setMobileSidebarOpen(false);
 
   return (
-    <div className="flex h-screen bg-gray-50">
-      {/* DESKTOP SIDEBAR */}
-      <div className="hidden lg:flex">
-        <Sidebar user={user} plan={user.plan || "free"} />
-      </div>
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      {/* APP */}
+      <div className="flex flex-1">
+        {/* DESKTOP SIDEBAR */}
+        <div className="hidden lg:flex">
+          <Sidebar user={user} plan={user.plan || "free"} />
+        </div>
 
-      {/* MOBILE OVERLAY */}
-      {mobileSidebarOpen && (
-        <div
-          className="
+        {/* MOBILE OVERLAY */}
+        {mobileSidebarOpen && (
+          <div
+            className="
             fixed
             inset-0
             z-40
             bg-black/50
             lg:hidden
           "
-          onClick={closeSidebar}
-        />
-      )}
+            onClick={closeSidebar}
+          />
+        )}
 
-      {/* MOBILE SIDEBAR */}
-      <div
-        className={`
+        {/* MOBILE SIDEBAR */}
+        <div
+          className={`
           fixed
           top-0
           left-0
@@ -58,32 +60,33 @@ export default function AppShell({
           ease-in-out
           ${mobileSidebarOpen ? "translate-x-0" : "-translate-x-full"}
         `}
-      >
-        <Sidebar
-          user={user}
-          plan={user.plan || "free"}
-          mobileOpen={mobileSidebarOpen}
-          onClose={closeSidebar}
-        />
-      </div>
+        >
+          <Sidebar
+            user={user}
+            plan={user.plan || "free"}
+            mobileOpen={mobileSidebarOpen}
+            onClose={closeSidebar}
+          />
+        </div>
 
-      {/* MAIN AREA */}
-      <div className="flex flex-1 flex-col overflow-hidden">
-        <AppHeader onMenuClick={() => setMobileSidebarOpen(true)} />
+        {/* MAIN */}
+        <div className="flex-1 flex flex-col">
+          <AppHeader onMenuClick={() => setMobileSidebarOpen(true)} />
 
-        <main
-          className="
+          <main
+            className="
             flex-1
-            overflow-auto
             p-4
             md:p-6
           "
-        >
-          {children}
-        </main>
-
-        <Footer />
+          >
+            {children}
+          </main>
+        </div>
       </div>
+
+      {/* FOOTER */}
+      <Footer />
     </div>
   );
 }
